ea167e9cdccf5ff4d074839ec2bd960189b94fc9
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 16:57:47 +0100

    connecting my server with postgres database + getting todos from db

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  database: process.env.DBNAME,
+});
+
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
 const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
+const pool = require("./db");
+
+app.get("/todos/:userEmail", async (req, res) => {
+  const userEmail = "yabad@test.42.fr";
+  try {
+    const todos = await pool.query(
+      "SELECT * FROM todos WHERE user_email = $1;",
+      [userEmail]
+    );
+    res.json(todos.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);

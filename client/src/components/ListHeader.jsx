de83ad94fd95ef90435e48be7fea8f1aeb30f26c
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 14:14:48 +0100

    Creating ListHeader component and styling it with css

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,7 +1,17 @@
 const ListHeader = ({ listName }) => {
+  const signOut = () => {
+    console.log("sign out");
+  };
+
   return (
     <div className="list-header">
       <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create">ADD NEW</button>
+        <button className="signout" onClick={signOut}>
+          SIGN OUT
+        </button>
+      </div>
     </div>
   );
 };

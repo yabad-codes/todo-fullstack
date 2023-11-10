ce27b3949f1227943db7a449f67e398175520e69
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 22:28:23 +0100

    Passing props to Modal to show it based on the mode ? true : false

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Modal from "./Modal";
 
 const ListHeader = ({ listName }) => {
+  const [showModal, setShowModal] = useState(false);
+
   const signOut = () => {
     console.log("sign out");
   };
@@ -9,12 +12,14 @@ const ListHeader = ({ listName }) => {
     <div className="list-header">
       <h1>{listName}</h1>
       <div className="button-container">
-        <button className="create">ADD NEW</button>
+        <button className="create" onClick={() => setShowModal(true)}>
+          ADD NEW
+        </button>
         <button className="signout" onClick={signOut}>
           SIGN OUT
         </button>
       </div>
-      <Modal />
+      {showModal && <Modal mode={"create"} setShowModal={setShowModal} />}
     </div>
   );
 };
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-const Modal = () => {
-  const mode = "create";
+const Modal = ({ mode, setShowModal }) => {
   const editMode = mode === "edit" ? true : false;
   const [data, setData] = useState({
     user_email: "",
@@ -22,7 +21,7 @@ const Modal = () => {
       <div className="modal">
         <div className="form-title-container">
           <h3>Let's {mode} your task</h3>
-          <button>X</button>
+          <button onClick={() => setShowModal(false)}>X</button>
         </div>
 
         <form>

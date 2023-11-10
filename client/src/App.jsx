9bdada64b1c6f67e5a58dd2c1fd5e24a7cf69dbf
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 18:28:44 +0100

    fetching the backend to get todos based on userEmail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const userEmail = "yabad@test.42.fr";
+  const [tasks, setTasks] = useState(null);
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/todos/${userEmail}`);
+      const json = await response.json();
+      setTasks(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => getData, []);
+  console.log(tasks);
+  // Sort by date
+
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
   return (
     <div className="app">
       <ListHeader listName={"Get shit done!"} />
+      {sortedTasks?.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
     </div>
   );
 };

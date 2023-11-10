a775a3effb752f90659527636ca28021647eea4c
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 22:33:35 +0100

    showing and closing modal for the edit button

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import ProgressBar from "./ProgressBar";
 import TickIcon from "./TickIcon";
+import Modal from "./Modal";
 const ListItem = ({ task }) => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <li className="list-item">
       <div className="info-container">
@@ -10,9 +13,14 @@ const ListItem = ({ task }) => {
       </div>
 
       <div className="button-container">
-        <button className="edit">EDIT</button>
+        <button className="edit" onClick={() => setShowModal(true)}>
+          EDIT
+        </button>
         <button className="delete">DELETE</button>
       </div>
+      {showModal && (
+        <Modal mode={"edit"} setShowModal={setShowModal} task={task} />
+      )}
     </li>
   );
 };

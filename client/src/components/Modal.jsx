6750ee6eda64e7d34cff247b934305667a043ce8
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 21:50:38 +0100

    Adding modal functionalities as a form

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,5 +1,42 @@
 const Modal = () => {
-  return <></>;
+  const mode = "create";
+
+  const handleChange = () => {
+    console.log("Something changed!");
+  };
+
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <div className="form-title-container">
+          <h3>Let's {mode} your task</h3>
+          <button>X</button>
+        </div>
+
+        <form>
+          <input
+            required
+            maxLength={30}
+            placeholder="Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            required
+            type="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={""}
+            onChange={handleChange}
+          />
+          <input className={mode} type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;

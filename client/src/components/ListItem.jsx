0506a77339ba0db72ec9c683d368a66179f23fb3
Yahya Abad <yabad@c2r8p6.1337.ma>
2023-11-10 18:30:01 +0100

    adding and styling both ListItem and TickIcon components

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,5 +1,20 @@
-const ListItem = () => {
-  return <></>;
+import ProgressBar from "./ProgressBar";
+import TickIcon from "./TickIcon";
+const ListItem = ({ task }) => {
+  return (
+    <li className="list-item">
+      <div className="info-container">
+        <TickIcon />
+        <p className="task-title">{task.title}</p>
+        <ProgressBar />
+      </div>
+
+      <div className="button-container">
+        <button className="edit">EDIT</button>
+        <button className="delete">DELETE</button>
+      </div>
+    </li>
+  );
 };
 
 export default ListItem;

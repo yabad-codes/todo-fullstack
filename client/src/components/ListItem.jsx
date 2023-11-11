f626dc09d18cda6b076f1eb53cc8c5273a046638
Yahya Abad <yabad@c1r5p10.1337.ma>
2023-11-11 12:32:14 +0100

    Sending delete request to the server to delete todos in the database

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -4,6 +4,21 @@ import TickIcon from "./TickIcon";
 import Modal from "./Modal";
 const ListItem = ({ task, getData }) => {
   const [showModal, setShowModal] = useState(false);
+
+  const deleteData = async () => {
+    const serverUrl = import.meta.env.VITE_REACT_APP_SERVERURL;
+    try {
+      const response = await fetch(`${serverUrl}/todos/${task.id}`, {
+        method: "DELETE",
+      });
+      if (response.status === 200) {
+        getData();
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <li className="list-item">
       <div className="info-container">
@@ -16,7 +31,9 @@ const ListItem = ({ task, getData }) => {
         <button className="edit" onClick={() => setShowModal(true)}>
           EDIT
         </button>
-        <button className="delete">DELETE</button>
+        <button className="delete" onClick={deleteData}>
+          DELETE
+        </button>
       </div>
       {showModal && (
         <Modal

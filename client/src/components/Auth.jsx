d12edcf72999e3bf410a147ff59ac7ee056f0c86
Yahya Abad <yabad@c1r5p10.1337.ma>
2023-11-12 09:48:01 +0100

    adding the bones of Auth component without any styling yet

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
 const Auth = () => {
-  return <></>;
+  const [isLogIn, setIsLogIn] = useState(true);
+  const [error, setError] = useState(null);
+
+  const viewLogin = (status) => {
+    setError(null);
+    setIsLogIn(status);
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
+        <form>
+          <h2>{isLogIn ? "Please log in" : "Please sign up"}</h2>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          {!isLogIn && <input type="password" placeholder="confirm password" />}
+          <input type="submit" className="create" />
+          {error && <p>{error}</p>}
+        </form>
+        <div className="auth-options">
+          <button
+            onClick={() => viewLogin(false)}
+            style={{
+              backgroundColor: !isLogIn ? "white" : "rgb(188, 188, 188)",
+            }}
+          >
+            Sign Up
+          </button>
+          <button
+            onClick={() => viewLogin(true)}
+            style={{
+              backgroundColor: !isLogIn ? "rgb(188, 188, 188)" : "white",
+            }}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;

c5bfcb83841ba08ee0bebe1426eabb6e53bb2e69
Yahya Abad <yabad@c1r5p10.1337.ma>
2023-11-12 12:00:10 +0100

    adding login and signup logic

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const cors = require("cors");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
 const pool = require("./db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(cors());
+
+// getting todos
 
 app.get("/todos/:userEmail", async (req, res) => {
   const { userEmail } = req.params;
@@ -20,6 +24,8 @@ app.get("/todos/:userEmail", async (req, res) => {
     console.error(err);
   }
 });
+
+// inserting todos
 
 app.post("/todos", async (req, res) => {
   const { user_email, title, progress, date } = req.body;
@@ -35,6 +41,8 @@ app.post("/todos", async (req, res) => {
   }
 });
 
+// updating todos
+
 app.put("/todos/:id", async (req, res) => {
   const { id } = req.params;
   const { user_email, title, progress, date } = req.body;
@@ -49,6 +57,8 @@ app.put("/todos/:id", async (req, res) => {
   }
 });
 
+// deleting todos
+
 app.delete("/todos/:id", async (req, res) => {
   const { id } = req.params;
   try {
@@ -56,6 +66,54 @@ app.delete("/todos/:id", async (req, res) => {
       id,
     ]);
     res.json(deleteToDo);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// signup
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  try {
+    const signUp = await pool.query(
+      "INSERT INTO users (email, hashed_password) VALUES($1, $2);",
+      [email, hashedPassword]
+    );
+
+    const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
+
+    res.json({ email, token });
+  } catch (err) {
+    console.error(err);
+    if (err) res.json({ detail: err.detail });
+  }
+});
+
+// login
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const users = await pool.query("SELECT * FROM users WHERE email = $1;", [
+      email,
+    ]);
+
+    if (!users.rows.length) return res.json({ detail: "User does not exist" });
+    const success = await bcrypt.compare(
+      password,
+      users.rows[0].hashed_password
+    );
+    const token = jwt.sign({ email }, "secret", { expiresIn: "1hr" });
+
+    if (success) {
+      res.json({ email: users.rows[0].email, token });
+    } else {
+      res.json({ detail: "Login failed, try again later please." });
+    }
   } catch (err) {
     console.error(err);
   }

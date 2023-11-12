63dafb655fb5a40fea884515d903b9886803d2a6
Yahya Abad <yabad@c1r5p10.1337.ma>
2023-11-12 11:31:57 +0100

    adding Authentication logic

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,12 +1,41 @@
 import { useState } from "react";
+import { useCookies } from "react-cookie";
+const serverUrl = import.meta.env.VITE_REACT_APP_SERVERURL;
 
 const Auth = () => {
   const [isLogIn, setIsLogIn] = useState(true);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(null);
 
   const viewLogin = (status) => {
     setError(null);
     setIsLogIn(status);
+  };
+
+  const handleSubmit = async (e, endpoint) => {
+    e.preventDefault();
+    if (!isLogIn && password !== confirmPassword) {
+      setError("Make sure passwords match!");
+      return;
+    }
+    const response = await fetch(`${serverUrl}/${endpoint}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+
+    if (data.detail) {
+      setError(data.detail);
+    } else {
+      setCookie("Email", data.email);
+      setCookie("AuthToken", data.token);
+      window.location.reload();
+    }
   };
 
   return (
@@ -14,10 +43,28 @@ const Auth = () => {
       <div className="auth-container-box">
         <form>
           <h2>{isLogIn ? "Please log in" : "Please sign up"}</h2>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          {!isLogIn && <input type="password" placeholder="confirm password" />}
-          <input type="submit" className="create" />
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {!isLogIn && (
+            <input
+              type="password"
+              placeholder="confirm password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          )}
+          <input
+            type="submit"
+            className="create"
+            onClick={(e) => handleSubmit(e, isLogIn ? "login" : "signup")}
+          />
           {error && <p>{error}</p>}
         </form>
         <div className="auth-options">
